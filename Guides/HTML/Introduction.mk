b12f7afbaadec434cd4935556f587a976d6c2222
MrShadow131 <laziorandom2020@gmail.com>
2026-05-13 22:14:12 -0300

    Update Introduction.mk

--- Guides/HTML/Introduction.mk
+++ Guides/HTML/Introduction.mk
@@ -1,1 +1,17 @@
-A
+# HTML
+HTML de las siglas Hyper Text Marking Language, o lenguaje de marcado de hipertexto, es un lenguaje, como bien dice su definicion, de marcado.
+Es el estandar utilizado para poder armar paginas y sitios web en el internet.
+
+## ¿Que es un lenguaje de marcado y en que se diferencia con un lenguaje de programación?
+Un lenguaje de marcado es aquel que se utiliza para codificar un documento utilizando etiquetas o elementos para establecer su estructura. El unico objetivo de un lenguaje de marcado es decirle al software como procesar nuestro documento.
+Y esa es la diferencia clave con un lenguaje de programacion: un lenguaje de programacion contiene logica en su ejecución; el lenguaje de marcado no tiene logica y solo le dice al software como procesar nuestro documento. A la hora de escribir codigo nosotros vemos el contenido y las etiquetas o elementos utilizados para presentar y procesar nuestro contenido, el usuario final solo vera el contenido final.
+Por ejemplo:
+En HTML si queremos marcar un texto en negrita colocamos el texto entre la etiqueta:
+'<strong>IMPORTANTE!</strong>'
+El usuario que vea el resultado final solo vera el texto en negrita:
+*IMPORTANTE!*
+
+## ¿Que se vera en esta guía?
+Se van a ver todas las etiquetas y elementos utilizados en HTML para poder saber como utilizar correctamente el lenguaje.
+Tambien se veran las convicciones a tomar para crear un buen codigo correcto, escalable y profesional (o se hara el intento jeje).
+Esta guia puede de ir de la mano con la guia de CSS y JavaScript ya que HTML con los dos anteriores forman a la triada de la web, una triada que sirve para poder formar un sitio web o pagina web de la forma mas completa, con diseño, estructura y funcionalidad.
